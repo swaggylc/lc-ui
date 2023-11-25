71260b0b8210aa71943b78be1a848982e36f8277
Lu Can <lucanlc0417@gmail.com>
2023-11-25 18:47:19 +0800

    在使用 <script setup> 的单文件组件中，导入的组件可以直接在模板中使用，无需注册

--- src/main.js
+++ src/main.js
@@ -1,28 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-// 导入组件
-import LcButton from "./components/button/LcButton.vue"
-import LcDialog from "./components/dialog/LcDialog.vue"
-import LcInput from "./components/input/LcInput.vue"
-import LcSwitch from './components/switch/LcSwitch.vue'
-import LcRadio from './components/radio/LcRadio.vue'
-import LcRadioGroup from './components/radio-group/LcRadioGroup.vue'
-import LcForm from './components/form/LcForm.vue'
-import LcFormItem from './components/form-item/LcFormItem.vue'
 // 全局引入图标文件
 import './assets/font/font.css'
 const app = createApp(App)
-
-app.component(LcButton, LcButton)
-app.component(LcDialog, LcDialog)
-app.component(LcInput, LcInput)
-app.component(LcSwitch, LcSwitch)
-app.component(LcRadio, LcRadio)
-app.component(LcRadioGroup, LcRadioGroup)
-app.component(LcForm, LcForm)
-app.component(LcFormItem, LcFormItem)
-
-
 
 
 
